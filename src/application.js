60c74c25d9895f4233c295a058a15b48f523ad39
Maksim Pavlov <hg-pavlov@mail.ru>
2019-05-01 19:13:22 +0300

    change logic

--- src/application.js
+++ src/application.js
@@ -2,8 +2,10 @@
 const fs = require("fs"),
 	path = require('path'),
 	restify = require('restify'),
-	errors = require('restify-errors'),
-	context = require('./components/context');
+	errors = require('restify-errors');
+
+let auth = require('./system/authenticate');
+let access = require('./system/access');
 
 class App
 {
@@ -12,6 +14,8 @@ class App
 		this.config = config;
 
 		this.server = restify.createServer(config.server);
+
+		this.server.pre(restify.plugins.pre.context());
 		this.server.use(restify.plugins.acceptParser(this.server.acceptable));
 		this.server.use(restify.plugins.authorizationParser());
 		this.server.use(restify.plugins.dateParser());
@@ -32,33 +36,45 @@ class App
 		);
 	}
 
-	getController (name)
+	loadElements (elementDir, elementSuffix, elementInit)
 	{
-		return typeof this.controllers[name] !== 'undefined' ? this.controllers[name] : null;
+		let componentsPath = path.join(__dirname, 'components');
+		fs.readdirSync(componentsPath)
+		.forEach((componentDir) => {
+			let componentPath = path.join(componentsPath, componentDir);
+			let elementPath = path.join(componentsPath, componentDir, elementDir);
+			let fstat = fs.statSync(componentPath);
+			if (!fstat.isDirectory() || componentDir.indexOf('.') === 0) return;
+			this[elementDir] = {};
+			fs.readdirSync(elementPath)
+			.forEach((file) => {
+				let elementFilePath = path.join(elementPath, file);
+				let fstat = fs.statSync(elementFilePath);
+				if (!fstat.isFile() || file.indexOf('.') === 0 || file.indexOf(elementSuffix) === -1) return;
+				let elementObj = require(elementFilePath),
+				keyRe = new RegExp('('+elementSuffix+'|\.js$)', 'ig'),
+				key = file.replace(keyRe,'');
+				this[elementDir][key] = new elementObj(this, key);
+				elementInit(this[elementDir][key]);
+			});
+		});
 	}
 
 	loadControllers ()
 	{
-		let controllersPath = path.join(__dirname, 'controllers');
-		this.controllers = {};
-		fs.readdirSync(controllersPath).forEach((file) => {
-			if (file.indexOf('.') === 0 || file.indexOf('Controller') === -1) return;
-			let controller = require(path.join(controllersPath, file)),
-			key = file.replace(/(Controller|\.js$)/ig,'');
-			this.controllers[key] = new controller(this, key);
-			this.setServerRouting(this.server, this.controllers[key].routes);
+		this.loadElements('controllers', 'Controller', async (element) => {
+			this.setServerRouting(this.server, element.routes);
 		});
 	}
-
 	setServerRouting (server, routes)
 	{
 		routes.forEach((routeObj) => {
 
 			server[routeObj.type]({ path: routeObj.path, name: routeObj.name||'' }, async (req, res, next) => {
-				// create context
-				let ctx = new context(routeObj.options);
-				if (ctx.accessIsAllowed()) {
-					routeObj.handler(ctx, req, res, next);
+				auth.authenticate(routeObj.options.auth||{}, req);
+				access.process(routeObj.options.access||{}, auth.getUser(req), req);
+				if (access.isAllowed(req)) {
+					routeObj.handler(req, res, next);
 				} else {
 					next(new errors.ForbiddenError());
 				}
