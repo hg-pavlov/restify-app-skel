75a59fa7b648c304f102a57101a2906b800a51b2
Maksim Pavlov <hg-pavlov@mail.ru>
2019-05-04 23:49:46 +0300

    append checking jwt access token

--- src/system/authenticate/index.js
+++ src/system/authenticate/index.js
@@ -1,8 +1,13 @@
 
-const errors = require('restify-errors'),
+const fs = require('fs'),
+	errors = require('restify-errors'),
 	rjwt = require('restify-jwt-community'),
 	jwt = require('jsonwebtoken');
 
+let keys = {
+	priv: fs.readFileSync(process.cwd()+'/config/jwt/private.key', 'utf8'),
+	pub: fs.readFileSync(process.cwd()+'/config/jwt/public.key', 'utf8')
+};
 
 class Authenticate
 {
@@ -13,26 +18,20 @@ class Authenticate
 
 	authenticate (options, req)
 	{
-		let user = null, credentials = null, repository = null;
+		let user = null, repository = this.app.getComponentRepository(options.repository);
 
-		// strategy function
-		let strategyType = options.strategy ? options.strategy.toLowerCase() : 'basic', strategyFn = null;
-		switch (strategyType) {
-			case 'basic': strategyFn = this.strategyBasic; break;
-		}
-		if (!strategyFn) throw new errors.InternalError('Authentication strategy "'+ strategyType +'" is not supported');
-		credentials = strategyFn(req);
-
-		// verification from repository (database)
+		// verification/update token from repository (database)
 		if (typeof options.repository === 'undefined')
 			throw new errors.InternalError('User repository must be defined credentials to be verified');
 
-		if (!credentials || typeof credentials.username === 'undefined' || typeof credentials.password === 'undefined')
-			throw new errors.InternalError('Credentials must be defined credentials to be verified');
-
-		repository = this.app.getComponentRepository(options.repository);
-
-		user = this.verification(repository, credentials);
+		// strategy function
+		let strategyType = options.strategy ? options.strategy.toLowerCase() : 'basic';
+		switch (strategyType) {
+			case 'basic': user = this.strategyBasic(req, repository, options); break;
+			case 'jwt': user = this.strategyJWT(req, repository, options); break;
+			default:
+				throw new errors.InternalError('Authentication strategy "'+ strategyType +'" is not supported');
+		}
 
 		req.set('auth.user', user);
 	}
@@ -42,8 +41,10 @@ class Authenticate
 		return req.get('auth.user');
 	}
 
-	strategyBasic (req)
+	strategyBasic (req, repository, options)
 	{
+		let user = null, credentials = null;
+
 		if (req.authorization
 			&& typeof req.authorization.scheme === 'string'
 			&& req.authorization.scheme.toLowerCase() === 'basic'
@@ -51,10 +52,13 @@ class Authenticate
 			&& typeof req.authorization.basic.username !== 'undefined'
 			&& typeof req.authorization.basic.password !== 'undefined'
 		) {
-			return req.authorization.basic;
+			credentials = req.authorization.basic;
 		} else {
 			throw new errors.UnauthorizedError('Authorized users only');
 		}
+
+		if (!credentials || typeof credentials.username === 'undefined' || typeof credentials.password === 'undefined')
+			throw new errors.InternalError('Credentials must be defined credentials to be verified');
 
 		/*
 		let credentials = null;
@@ -77,11 +81,44 @@ class Authenticate
 		}
 		return credentials;
 		*/
+
+		user = repository.findOne(credentials);
+
+		return user;
+	}
+	strategyJWT (req, repository, options)
+	{
+		let user = null, token = null;
+
+		if (req.authorization
+			&& typeof req.authorization.scheme === 'string'
+			&& req.authorization.scheme.toLowerCase() === 'bearer'
+			&& typeof req.authorization.credentials === 'string'
+		) {
+			token = req.authorization.credentials;
+		} else {
+			throw new errors.UnauthorizedError('Authorized users only');
+		}
+
+		if (!token)
+			throw new errors.UnauthorizedError('JWT must be defined to be verified');
+
+		user = jwt.verify(token, keys.pub);
+
+		return user;
 	}
 
-	verification (repository, credentials)
+	jwtCreate (credentials, repository, options, ttl)
 	{
-		return repository.findOne(credentials);
+		let user = repository.findOne(credentials), data = {};
+		if (!user) throw new errors.NotFoundError('User not found');
+
+		data.user = user;
+
+		return jwt.sign(data, keys.priv, {
+			algorithm: 'RS256',
+            expiresIn: ttl||'15m'
+        });
 	}
 }
 
